4d7064d377607bb8ce0ca9406233195514734137
razag <darkkebab@gmail.com>
2023-03-29 17:02:42 +0200

    Add env variable for serverUrl

--- src/stores/globals.ts
+++ src/stores/globals.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useGlobalsStore = defineStore('globals', {
   state: () => ({
-    serverUrl: 'http://localhost:3000',
+    serverUrl: import.meta.env.VITE_BACKEND_HOST,
     clientUrl: 'http://localhost:5173',
   }),
   getters: {
